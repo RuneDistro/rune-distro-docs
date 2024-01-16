0bafc117384b66cd75b7327e8f259b140e1dd49b
Pietro Rhyan <pietroqjg@gmail.com>
2024-01-16 00:46:04 -0300

    Inital Config

---
+++ docs/intro.md
@@ -0,0 +1,9 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Introdução
+
+Aqui será onde iremos documentar todo o nosso processo de contrução de software, tanto o FrontEnd, as Documentações e
+os provavéis requisitos funcionais e não-funcionais.
---
+++ docusaurus.config.ts
@@ -0,0 +1,116 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Rune Distro',
+  tagline: 'Rune Distro Documentation',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Rune Distro', // Usually your GitHub org/user name.
+  projectName: 'rune-distro-docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/RuneDistro/rune-distro-docs',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Rune Distro',
+      logo: {
+        alt: 'Rune Distro Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Introdução',
+        },
+        {
+          href: 'https://github.com/RuneDistro',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Introdução',
+              to: '/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+
+            {
+              label: 'Twitter - X',
+              href: 'https://twitter.com/pietrorhyan2',
+            },
+          ],
+        },
+        {
+          title: 'Mais',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/RuneDistro',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Rune Distro, Inc. Desenvolvida com paixão.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
